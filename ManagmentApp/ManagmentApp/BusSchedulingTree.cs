ff8545f6257f465ed448057fe407e298fc3cc0d8
AlexS <alex.shemesh@gmail.com>
2011-06-29 00:11:32 +0300

    h

---
+++ ManagmentApp/ManagmentApp/BusSchedulingTree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ManagmentApp
+{
+  public partial class BusSchedulingTree : Form
+  {
+    public BusSchedulingTree()
+    {
+      InitializeComponent();
+    }
+  }
+}
